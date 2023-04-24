91fd93d43e27fdaded8481c77c811bff45926578
Emily McLean <emilymclean93@gmail.com>
2023-04-24 18:58:51 -0400

    cleaned up code in shapes.js and finished setShape method in svg.js

--- assets/lib/shapes.js
+++ assets/lib/shapes.js
@@ -1,54 +1,38 @@
 const CLI = require('../../index.js')
 
 class Shape {
-    constructor(text, textColor, shapeColor) {
-        this.text = text;
-        this.textColor = textColor;
+    constructor(shapeColor) {
         this.shapeColor = shapeColor;
     }
 }
 
 class Circle extends Shape {
-    constructor(text, textColor, shapeColor) {
-        super(text, textColor, shapeColor);
+    constructor(shapeColor) {
+        super(shapeColor);
     }
 
     render() {
-        return `
-    <svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
-        <circle cx="150" cy="100" r="80" fill="green" />
-        <text x="150" y="125" font-size="60" text-anchor="middle" fill="white">SVG</text>
-    </svg>
-    `
+        return `<circle cx="150" cy="100" r="80" fill="${this.shapeColor}" />`
     }
 };
 
 class Triangle extends Shape {
-    constructor(text, textColor, shapeColor) {
-        super(text, textColor, shapeColor);
+    constructor(shapeColor) {
+        super(shapeColor);
     }
 
     render() {
-        return `
-            <svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
-                 <polygon points="150, 18 244, 182 56, 182" fill="bisque" />
-                 <text x="150" y="150" font-size="60" text-anchor="middle" fill="#3a0467">SVG</text>
-             </svg>`;
+        return `<polygon points="150, 18 244, 182 56, 182" fill="${this.shapeColor}" />`;
     }
-
 };
 
 class Square extends Shape {
-    constructor(text, textColor, shapeColor) {
-        super(text, textColor, shapeColor);
+    constructor(shapeColor) {
+        super(shapeColor);
     }
 
     render() {
-        return `
-        <svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
-            <rect x="90" y="40" width="120" height="120" fill="turquoise" />
-  <         text x="150" y="125" font-size="60" text-anchor="middle" fill="#444">SVG</>
-        </svg>`;
+        return `<rect x="90" y="40" width="120" height="120"fill="${this.shapeColor}" />`;
     }
 };
 
--- assets/lib/svg.js
+++ assets/lib/svg.js
@@ -10,25 +10,22 @@ class SVG {
 
         // template literal for shapes/color
         // switch case
-   
-    }
 
-    // We need to create a setText method that takes in a message and a color and sets the textElement property to a string that represents a text element, using literal to pass in the message and color.
+    }
 
     setText(text, textColor) {
         if (text.length > 3) {
             throw new Error('Text cannot exceed 3 characters dummy!');
         }
 
-        this.textElement = `<text x="150" y="125" font-size="60" text-anchor="middle" fill="${textColor}">${text}</text>`
+        this.textElement = `<text x="150" y="125" font-size="60" text-anchor="middle" fill="${textColor}">${text}</text>`;
     }
 
-    // We need to create a setShape method that takes in a shape and sets the shapeElement property to the shape's render method.
 
     setShape() {
-        this.shapeElement = 
+        this.shapeElement = shape.render();
     }
-    
+
 }
 
 module.exports = SVG;
